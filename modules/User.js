1f073b8dda9310268800a99b5157e6e8de96bcc8
magiclydev <69756639+magiclydev@users.noreply.github.com>
2025-01-18 20:33:16 +0000

    Create User.js

---
+++ modules/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+        type: String,
+        enum: ['Super Admin', 'Admin', 'Manager'],
+        default: 'Admin',
+    },
+});
+
+// Password hashing before saving
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
