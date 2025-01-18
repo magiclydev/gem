ae11f50c5612b6518d7cf6ad74168ee9d49dc355
magiclydev <69756639+magiclydev@users.noreply.github.com>
2025-01-18 20:42:19 +0000

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const adminRoutes = require('./routes/admin.js');
 // ... require other models (Question, TrialCode, etc.)
 
 // Authentication middleware (protect admin routes)
