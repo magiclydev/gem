46b3c628cbc613092ea2a0549d5651bf7c616b64
magiclydev <69756639+magiclydev@users.noreply.github.com>
2025-01-18 20:32:49 +0000

    Create exam.js

---
+++ routes/exam.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+// ... require models (Exam, Question, etc.)
+
+// ... exam routes for taking exams, submitting, and getting results
+
+module.exports = router;
